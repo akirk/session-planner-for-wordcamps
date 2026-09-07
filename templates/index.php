38958f747c0e0842e71e727b75b648b939c6b42d
Alex Kirk <akirk@users.noreply.github.com>
2026-09-07 17:22:45 +0200

    Address plugin checker template warnings

--- templates/index.php
+++ templates/index.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound -- Template variables are render-local state.
 defined( 'ABSPATH' ) || exit;
 ?>
 <!DOCTYPE html>
